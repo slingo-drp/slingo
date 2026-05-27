cf71a95855f2b41fc361ba31433e4ba461363a30
Ahmed Elsayed <ahmeding.hosam@gmail.com>
2026-05-27 01:34:41 +0100

    chore: setup nativewind + eslint

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,15 +1,7 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
-import { useColorScheme } from 'react-native';
+import "../../global.css";
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
+import { Stack } from "expo-router";
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
-  );
+export default function RootLayout() {
+  return <Stack />;
 }
